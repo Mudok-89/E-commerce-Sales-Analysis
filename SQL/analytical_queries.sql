29bbe4430552bfe5c18bf4ce4e3c40be58a501c2
Pavla Šindlerova <pavla.sindlerova@gmail.com>
2026-09-10 16:53:04 +0200

    Update analytical_queries.sql

--- SQL/analytical_queries.sql
+++ SQL/analytical_queries.sql
@@ -1,9 +1,9 @@
 -- 1.	Napište SQL dotaz, kterým ověříte, zda jsou v tabulce Products nějaké záznamy se stejným ProductName. (3 body) 
 
 SELECT 
- 	ProductName,
- 	COUNT(*) AS  Počet
-FROM Products
+ 	p.ProductName,
+ 	COUNT(*) AS  DuplicateCount
+FROM Products AS p
 GROUP BY ProductName    -- seskupí řádky podle názvu produktu
 HAVING COUNT(*) > 1;        -- nechá jen ty názvy, které se opakují(duplicity)
 
@@ -15,14 +15,14 @@ HAVING COUNT(*) > 1;        -- nechá jen ty názvy, které se opakují(duplicit
 -- 2.	Napište SQL dotaz, kterým pro každou zemi (Country) spočítejte celkovou cenu objednávek. Vyberte ty země, kde celková cena je větší než 100 a seřaďte je podle celkové ceny. (5 bodů) 
 
 SELECT 
-	Country,
+	c.Country,
 	SUM(od.Quantity * p.Price) AS TotalOrderPrice 
-FROM Customers c
-JOIN Orders o
+FROM Customers AS c
+INNER JOIN Orders  AS o
     ON c.CustomerID = o.CustomerID
-JOIN OrderDetails od
+INNER JOIN OrderDetails AS od
     ON o.OrderID = od.OrderID
-JOIN Products p
+INNER JOIN Products AS p
     ON od.ProductID = p.ProductID
 GROUP BY Country
 HAVING 
@@ -41,74 +41,92 @@ SELECT
  	COUNT(DISTINCT CASE
  	WHEN c.CategoryName = 'Seafood'
  	THEN o.OrderID
- 	END) AS SeafoodOrders,
+ 	END) AS SeafoodOrdersCount,
 
  	COUNT(DISTINCT CASE
  	WHEN c.CategoryName = 'Beverages'
  	THEN o.OrderID
- 	END) AS BeveragesOrders
+ 	END) AS BeveragesOrdersCount
 
-FROM Shippers s
-JOIN Orders o
+FROM Shippers AS s
+INNER JOIN Orders AS o
     ON s.ShipperID = o.ShipperID
-JOIN OrderDetails od
+INNER JOIN OrderDetails AS od
     ON o.OrderID = od.OrderID
-JOIN Products p
+INNER JOIN Products AS p
     ON od.ProductID = p.ProductID
-JOIN Categories c
+JOIN Categories AS c
     ON p.CategoryID = c.CategoryID
 
-WHERE YEAR(o.OrderDate) = 1997
-GROUP BY s.ShipperName
-ORDER BY s.ShipperName;
+WHERE o.OrderDate >= '1997-01-01' 
+AND o.OrderDate < '1998-01-01'
+
+GROUP BY 
+	s.ShipperName
+ORDER BY 
+	s.ShipperName;
 
 --Je nutné použít DISTINCT, protože jedna objednávka může mít více řádků v OrderDetails. U dopravce Speedy Express by v kategorii SeafoodOrders vycházel počet objednávek 7 místo 5ti.
 -- Bez DISTINCT bychom počítali spíš počet položek objednávek, ne skutečný počet objednávek. 
 
 -- 5a) Kteří zaměstnanci v roce 1997 vygenerovali nejvyšší tržby, největší počet objednávek a jaká byla průměrná hodnota objednávky?
 
+WITH OrderValues AS (
+    SELECT
+        o.OrderID,
+        o.EmployeeID,
+        SUM(od.Quantity * p.Price) AS OrderValue
+    FROM Orders AS o
+    INNER JOIN OrderDetails AS od
+        ON o.OrderID = od.OrderID
+    INNER JOIN Products AS p
+        ON od.ProductID = p.ProductID
+    WHERE o.OrderDate >= '1997-01-01'
+      AND o.OrderDate < '1998-01-01'
+    GROUP BY
+        o.OrderID,
+        o.EmployeeID
+)
+
 SELECT
+    e.EmployeeID,
     e.FirstName,
     e.LastName,
-    COUNT(DISTINCT o.OrderID) AS PocetObjednavek,
-    SUM(od.Quantity * p.Price) AS CelkovaTrzba,
-    SUM(od.Quantity * p.Price) / COUNT(DISTINCT o.OrderID) AS PrumernaHodnotaObjednavky
- 
-FROM Employees e
-JOIN Orders o
-    ON e.EmployeeID = o.EmployeeID
-JOIN OrderDetails od
-    ON o.OrderID = od.OrderID
-JOIN Products p
-    ON od.ProductID = p.ProductID
-WHERE YEAR(o.OrderDate) = 1997
+    COUNT(ov.OrderID) AS OrderCount,
+    SUM(ov.OrderValue) AS TotalSales,
+    AVG(ov.OrderValue) AS AverageOrderValue
+FROM Employees AS e
+INNER JOIN OrderValues AS ov
+    ON e.EmployeeID = ov.EmployeeID
 GROUP BY
     e.EmployeeID,
     e.FirstName,
     e.LastName
 ORDER BY
-    CelkovaTrzba DESC;
+    TotalSales DESC;
+
 
 
 -- 5b) Které produktové kategorie mají nejvyšší tržby a kolik kusů se v nich prodalo?
 
 SELECT
-    cat.CategoryName,
-    COUNT(DISTINCT o.OrderID) AS PocetObjednavek,
-    SUM(od.Quantity) AS ProdaneMnozstvi,
-    SUM(od.Quantity * p.Price) AS CelkovaTrzba,
-    AVG(p.Price) AS PrumernaCenaProduktu
-FROM Categories cat
-JOIN Products p 
-    ON cat.CategoryID = p.CategoryID
-JOIN OrderDetails od 
-    ON p.ProductID = od.ProductID
-JOIN Orders o 
-    ON od.OrderID = o.OrderID
-WHERE YEAR(o.OrderDate) = 1997
-GROUP BY 
-    cat.CategoryName
+    	 c.CategoryName,
+   	 COUNT(DISTINCT o.OrderID) AS OrderCount,
+    	 SUM(od.Quantity) AS UnitSold,
+   	 SUM(od.Quantity * p.Price) AS TotalSales
+FROM Categories AS c
+INNER JOIN Products AS p 
+    	ON c.CategoryID = p.CategoryID
+INNER  JOIN OrderDetails AS od 
+ 	ON p.ProductID = od.ProductID
+INNER  JOIN Orders AS o 
+    	ON od.OrderID = o.OrderID
+WHERE o.OrderDate >= '1997-01-01' 
+      AND o.OrderDate < '1998-01-01' 
+GROUP BY  
+c.CategoryName
 ORDER BY 
-    CelkovaTrzba DESC;
+ 	 TotalSales DESC;
+
 
 
